d3d24f7e0fb263197bc57a55d721844a37b22cfc
Constanza Arriaza <co.arriaza@duocuc.cl>
2026-09-02 00:02:58 -0400

    agregando js de productos

---
+++ productos.js
@@ -0,0 +1,38 @@
+let productos = [
+
+    {
+        nombre: "Bajo Eléctrico Jazz Bass",
+        categoria: "Bajos Eléctricos",
+        marca: "Fender",
+        modelo: "Player Jazz",
+        precio: 699990,
+        stock: 2,
+        imagen: "imagenes/tarjetas/Bajo Fender Player Jazz.jpg",
+        descripcion: "Alder body, 2 Alnico V Jazz single-coil."
+    },
+
+
+    {
+        nombre: "Guitarra Acústica Folk",
+        categoria: "Guitarras Acústicas",
+        marca: "Yamaha",
+        modelo: "F310",
+        precio: 129990,
+        stock: 8,
+        imagen: "imagenes/tarjetas/Guitarra Acustica Yamaha F310.webp",
+        descripcion: "Tapa de abeto, aros y fondo de meranti. Ideal para iniciantes."
+    },
+
+    {
+        nombre: 'Caja Snare 14"',
+        categoria: "Baterías",
+        marca: "Pearl",
+        modelo: "STE1450",
+        precio: 89990,
+        stock: 4,
+        imagen: "imagenes/tarjetas/Bateria Pearl STE1450.jpeg",
+        descripcion: 'Acero, 14x5", 10 tensores.'
+    }
+
+
+];
